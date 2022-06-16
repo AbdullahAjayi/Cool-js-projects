fe785b0f47ee8ea5357a87f1d338d657ff644ddf
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-06-16 00:49:33 -1100

    added another project: random choice picker

---
+++ Random-choice-picker/script.js
@@ -0,0 +1,68 @@
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(()=> {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
+})
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.textContent = tag;
+        tagsEl.appendChild(tagEl)
+    })
+}
+
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickrandom()
+
+        highlightTag(randomTag)
+
+        setTimeout(()=> {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100);
+
+    setTimeout(()=> {
+        clearInterval(interval)
+
+        setTimeout(()=> {
+            const randomTag = pickrandom()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickrandom() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
