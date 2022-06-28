dcf7b05c6d78506e8a853ac81ae8a7154de7e952
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-06-28 11:22:12 +0100

    made clock functional usng js

---
+++ clock-app/app.js
@@ -0,0 +1,30 @@
+const digitalClock = document.querySelector('.digital-clock')
+const hourHand = document.querySelector('.hour-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const secondHand = document.querySelector('.second-hand')
+
+
+setInterval(getDate, 1000);
+
+
+
+function getDate() {
+    const date = new Date();
+    
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let day_time = 'AM'
+
+    let hourDegree = (hours / 12 * 360) 
+    let minuteDegree = (minutes / 60 * 360)
+    let secondDegree = (seconds / 60 * 360)
+    
+    hourHand.style.transform = `rotate(${hourDegree}deg)`
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`
+    secondHand.style.transform = `rotate(${secondDegree}deg)`
+
+    digitalClock.textContent = `${hours}: ${minutes}: ${seconds} ${day_time}`
+}
+
+getDate()
