0178df1459d7644fea81b635860caba1c910e1c9
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-06-29 10:28:43 +0100

    added a new folder: countdown clock

---
+++ Countdown-clock/app.js
@@ -0,0 +1,9 @@
+function timer(seconds) {
+    const now = Date.now()
+    const then = now + seconds * 1000
+    
+    setInterval(()=>{
+        const secondsLeft = Math.round((then - Date.now()) / 1000)
+        console.log(secondsLeft);
+    }, 1000)
+}
