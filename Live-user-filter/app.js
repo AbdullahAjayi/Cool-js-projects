7b41e98fca38e94934d5cfca8f8d6c8a71137bbf
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-08-08 16:50:06 +0100

    formatted code with prettier exension

--- Live-user-filter/app.js
+++ Live-user-filter/app.js
@@ -1,44 +1,42 @@
-const result = document.getElementById('result')
-const filter = document.getElementById('filter')
-const listItems = []
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
+const listItems = [];
 
-getData()
+getData();
 
-filter.addEventListener('input', (e)=> filterData(e.target.value))
+filter.addEventListener("input", (e) => filterData(e.target.value));
 
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
+  const res = await fetch("https://randomuser.me/api?results=50");
+  const data = await res.json();
 
-    const data = await res.json()
+  // Clear Results
+  result.innerHTML = "";
+  const results = data.results;
 
-    // Clear Results
-    result.innerHTML = ''
-    const results = data.results;
+  results.forEach((user) => {
+    const li = document.createElement("li");
 
-    results.forEach(user => {
-        const li = document.createElement('li')
+    listItems.push(li);
 
-        listItems.push(li)
-
-        li.innerHTML = `
+    li.innerHTML = `
         <img src="${user.picture.large}" alt="${user.name.first}">
         <div class="user-info">
             <h4>${user.name.first} ${user.name.last}</h4>
             <p>${user.location.city}, ${user.location.country}</p>
         </div>
-        `
+        `;
 
-        result.appendChild(li)
-    })
+    result.appendChild(li);
+  });
 }
 
 function filterData(searchTerm) {
-    listItems.forEach(item => {
-        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
-            item.classList.remove('hide')
-        }
-        else {
-            item.classList.add('hide')
-        }
-    })
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
 }
