cd9f2fda6012cffdbea835aa48dc16b912e4e632
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-05-10 22:13:18 +0100

    Updated gsap project

--- gsap-project/script.js
+++ gsap-project/script.js
@@ -1,22 +1,24 @@
-// const blueBg = document.querySelector(".blue-bg");
-// const h1Text = document.querySelectorAll(".heading");
-// const text = document.querySelector(".text");
+const leftDiv = document.querySelector(".left-div-container");
+const h1Text = document.querySelectorAll(".h1-text");
+const card = document.querySelectorAll(".card");
+const navEl = document.querySelectorAll(".nav-el");
 
-// const tl = gsap.timeline();
+const tl = gsap.timeline();
 
-// tl.from(blueBg, {
-//   duration: 1,
-//   y: 100,
-// });
+tl.from(leftDiv, {
+  duration: 0.5,
+  y: 100,
+});
 
-// tl.from(h1Text, {
-//   opacity: 0,
-//   x: 100,
-// });
+tl.from(h1Text, {
+  opacity: 0,
+  x: 100,
+});
 
-// tl.from(text, {
-//   duration: 0.5,
-//   opacity: 0,
-//   y: -50,
-//   ease: "bounce.out",
-// });
+tl.from(card, {
+  duration: 0.5,
+  opacity: 0,
+  y: -50,
+  ease: "elastic",
+  stagger: 0.5,
+});
