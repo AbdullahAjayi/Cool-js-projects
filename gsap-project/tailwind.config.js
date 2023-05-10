ca1b4b804e1623f113cb970845586f3289552753
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-05-10 12:39:00 +0100

    Updated gsap-project

---
+++ gsap-project/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        BungeeOutline: ['"Bungee Outline"', "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
